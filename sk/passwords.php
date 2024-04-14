022495adb646a73d68217ad930971cb97589171c
evan70 <ivanpaldan@yandex.com>
2024-04-14 23:56:28 +0200

    password slovak language file

---
+++ sk/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Vaše heslo bolo obnovené!',
+    'sent' => 'Poslali sme vám e-mail s odkazom na obnovenie hesla!',
+    'throttled' => 'Predtým, než to skúsite znova, počkajte prosím.',
+    'token' => 'Tento token na obnovenie hesla je neplatný.',
+    'user' => 'Nemôžeme nájsť používateľa s touto e-mailovou adresou.',
+
+];
